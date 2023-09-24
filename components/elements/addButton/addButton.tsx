d27717a1facf281e2e7cbbeca30cda790da2da42
hikari <tanimoto0709@gmail.com>
2023-09-24 15:38:12 +0900

    update: 追加ボタンのスタイル修正

--- components/elements/addButton/addButton.tsx
+++ components/elements/addButton/addButton.tsx
@@ -4,24 +4,26 @@ import React, { useState } from 'react';
 
 const AddButton = () => {
   return (
-    <form
-      action=""
-      className="p-2 grid grid-cols-8 fixed left-0 bottom-4"
-    >
-      <input
-        type="text"
-        placeholder="タイトルを入力してください"
-        className="border border-gray-300 mr-2 p-2 col-span-3"
-      />
-      <input
-        type="number"
-        placeholder="読み終えた巻数を入力してください"
-        className="border border-gray-300 p-2 mr-2 col-span-3"
-      />
-      <button className="p-2 bg-blue-500 rounded cursor-pointer text-white col-span-2">
-        マンガを追加する
-      </button>
-    </form>
+    <div className="max-w-screen-sm fixed left-2/4 -translate-x-2/4 bottom-4 w-full p-2">
+      <form
+        action=""
+        className="grid grid-cols-8 gap-2 place-items-center"
+      >
+        <input
+          type="text"
+          placeholder="タイトルを入力してください"
+          className="border border-gray-300 p-2 col-span-3 w-full"
+        />
+        <input
+          type="number"
+          placeholder="読み終えた巻数を入力してください"
+          className="border border-gray-300 p-2 col-span-3 w-full"
+        />
+        <button className="p-2 bg-blue-500 rounded cursor-pointer text-white col-span-2 w-full">
+          マンガを追加する
+        </button>
+      </form>
+    </div>
   );
 };
 
--- components/elements/list/listItem.tsx
+++ components/elements/list/listItem.tsx
@@ -36,7 +36,7 @@ const ListItem = ({ key, magazineTitle, magazineNumberOfTurns }: Props) => {
       className="grid grid-cols-8 place-items-center border-b border-gray-300 mb-2 cursor-pointer"
     >
       <p className="col-span-5 p-2">{magazineTitle}</p>
-      <p className=" p-2">{count}</p>
+      <p className="p-2">{count}</p>
       <button
         onClick={decrement}
         className="w-full h-full col-span-1 bg-red-200 cursor-pointer p-2"
